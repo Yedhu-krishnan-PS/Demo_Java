2be4b385072c8388ed784e48269ea137d3f4e369
YEDHUKRISHNAN <yedhuskrishnan@gmail.com>
2025-01-24 16:51:10 +0530

    test

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        class001 abc = new class001();
+        System.out.println("Hello, World!");
+
+        String a = abc.main("qwert");
+        System.out.println(a);
+        System.out.println(class001.test("123"));
+    }
+}
