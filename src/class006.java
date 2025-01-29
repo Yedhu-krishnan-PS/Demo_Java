a97a8223a004961418475e900fd08d6f1955b092
YEDHUKRISHNAN <yedhuskrishnan@gmail.com>
2025-01-29 09:45:46 +0530

    class sum

---
+++ src/class006.java
@@ -0,0 +1,10 @@
+public class class006 {
+    public static void main(String[] args) {
+        int a = 35;
+        double b = 15;
+        short c = 49;
+
+        System.out.println(a+b);
+        System.out.println(a+b-c);
+    }
+}
---
+++ src/class007.java
@@ -0,0 +1,12 @@
+public class class007 {
+    public static void main(String[] args) {
+        String c = "Yedhu";
+        String b = "Krishnan";
+
+        System.out.println(c + b);
+        System.out.println(c + " " + b);
+        System.out.println(String.format("%s %s",c,b));
+
+
+    }
+}
