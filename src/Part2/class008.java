8ce524c1bb5ea400eae9b326598e2896bc9e4f4a
YEDHUKRISHNAN <yedhuskrishnan@gmail.com>
2025-01-30 17:09:09 +0530

    class sum

---
+++ src/Part2/class008.java
@@ -0,0 +1,15 @@
+package Part2;
+
+public class class008 {
+    public static void main(String[] args) {
+        double n = 67;
+        int d = 33;
+        short k = 1;
+
+        System.out.println(n+d+k);
+        System.out.println(n-k+d);
+        System.out.println(d+k-n);
+        System.out.println(k-n-d);
+        }
+    }
+
