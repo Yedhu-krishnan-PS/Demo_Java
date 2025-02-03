302606e63d26fbf9252c7e14dc5caa55bc57e3cd
YEDHUKRISHNAN <yedhuskrishnan@gmail.com>
2025-02-03 14:57:33 +0530

    boolean

---
+++ src/Part2/class010.java
@@ -0,0 +1,14 @@
+package Part2;
+
+public class class010 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        boolean b = (7==15);
+        boolean c = (a==7);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
