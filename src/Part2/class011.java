9d735e7f2b44f745ef88ce93748db5a81713f582
YEDHUKRISHNAN <yedhuskrishnan@gmail.com>
2025-02-03 15:38:33 +0530

    boolean

---
+++ src/Part2/class011.java
@@ -0,0 +1,24 @@
+package Part2;
+
+public class class011 {
+    public static void main(String[] args) {
+
+        boolean b1 = false;
+        boolean b2 = true;
+        boolean b3 = (b1==b2);
+        boolean b4 = (b1!=b2);
+
+        // b1 is false
+        System.out.println(b1);
+
+        // b2 is true
+        System.out.println(b2);
+
+        // b3 is false
+        System.out.println(b3);
+
+        // b4 is true
+        System.out.println(b4);
+
+    }
+}
---
+++ src/Part2/class012.java
@@ -0,0 +1,25 @@
+package Part2;
+
+public class class012 {
+    public static void main(String[] args) {
+
+        // initialized 'a' and 'b'
+        int a = 20;
+        int b = 30;
+
+        // initialized boolean variables 'a1' and 'b1'
+        boolean a1 = true;
+        boolean b1 = false;
+
+        /*
+         *  if condition starts here. If this condition matches
+         *  then 'a1' will be printed.
+         */
+
+        if ( b > a){
+            System.out.println(a1);
+        } else{
+            System.out.println(b1);
+        }
+    }
+}
